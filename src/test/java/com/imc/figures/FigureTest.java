1ba5bda76fe8f360de861d151c2dbebc3fa018eb
Evgeny Timofeev <evgeny.timofeev88@gmail.com>
2020-07-18 14:05:53 +0200

    tests

---
+++ src/test/java/com/imc/figures/FigureTest.java
@@ -0,0 +1,20 @@
+package com.imc.figures;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FigureTest {
+
+    private final static Rock ROCK = new Rock();
+    private final static Paper PAPER = new Paper();
+    private final static Scissors SCISSORS = new Scissors();
+
+    @Test
+    void fightLogic(){
+        assertTrue(SCISSORS.isBeated(ROCK));
+        assertTrue(ROCK.isBeated(PAPER));
+        assertTrue(PAPER.isBeated(SCISSORS));
+    }
+
+}
